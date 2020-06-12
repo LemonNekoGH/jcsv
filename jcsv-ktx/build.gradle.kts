0b7ae2489a9f379ff7c670d915f2236cf6d3db65
LemonNeko <chheese048@gmail.com>
2020-06-13 01:40:24 +0800

    干脆就不要发布到maven仓库了

--- jcsv-ktx/build.gradle.kts
+++ jcsv-ktx/build.gradle.kts
@@ -1,5 +1,3 @@
-import java.net.URI
-import java.util.Properties
 import java.io.File
 
 plugins {
@@ -23,36 +21,12 @@ repositories {
     mavenCentral()
     jcenter()
     maven {
-        url = URI.create("https://packages.aliyun.com/maven/repository/2008850-snapshot-0dxuho/")
-    }
-    maven {
-        url = URI.create("https://packages.aliyun.com/maven/repository/2008850-release-0dxuho/")
-    }
-    maven {
         url = repoDir.toURI()
     }
 }
 
 publishing {
     repositories {
-        maven {
-            credentials {
-                val prop = file("publish.properties")
-                val p = Properties()
-                prop.inputStream().apply {
-                    p.load(this)
-                }
-                username = p["username"] as String
-                password = p["password"] as String
-            }
-            val releaseUrl = "https://packages.aliyun.com/maven/repository/2008850-release-0dxuho/"
-            val snapshotUrl = "https://packages.aliyun.com/maven/repository/2008850-snapshot-zqKbkn/"
-            url = if (theVersion.endsWith("-SNAPSHOT")) {
-                URI.create(snapshotUrl)
-            } else {
-                URI.create(releaseUrl)
-            }
-        }
         maven {
             url = repoDir.toURI()
         }
